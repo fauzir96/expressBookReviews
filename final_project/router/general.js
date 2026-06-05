92f78385e307dc39ce466bdfbc9f6bc0f6bf669c
fauzir96 <ahmadfauzirohman@outlook.com>
2026-06-04 23:36:07 -0400

    Finishing axios

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -25,7 +25,7 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
+public_users.get('/',async function (req, res) {
   //Write your code here - Task 1 / 10
   try {
     const response = await axios.get('https://raw.githubusercontent.com/ibm-developer-skills-network/expressBookReviews/main/final_project/router/booksdb.js');
@@ -36,7 +36,7 @@ public_users.get('/',function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn',async function (req, res) {
   //Write your code here - Task 2 / 11
   const isbn = req.params.isbn;
   try {
@@ -52,15 +52,6 @@ public_users.get('/isbn/:isbn',function (req, res) {
     return res.status(500).json({ message: "Error fetching book details" });
   }
   });
-
-  getBookByISBN
-    .then((bookDetails) => {
-      return res.status(200).json(bookDetails);
-    })
-    .catch((error) => {
-      return res.status(404).json({ message: error });
-    });
-});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
